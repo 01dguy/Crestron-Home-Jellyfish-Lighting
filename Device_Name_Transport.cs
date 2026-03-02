a6fb523fb4ee3e98a50c72799124117049b276ac
Trey Nichols <racynichols@gmail.com>
2026-03-02 17:21:32 -0600

    Align protocol validation with provided JellyFish API spec

--- Device_Name_Transport.cs
+++ Device_Name_Transport.cs
@@ -1,81 +1,83 @@
-﻿using Crestron.RAD.Common.Transports;
+﻿using System;
+using Crestron.RAD.Common.Transports;
 
-namespace Home_Extension_Template
+namespace JellyfishLighting.ExtensionDriver
 {
 	public class Device_Name_Transport : ATransportDriver
 	{
-		#region Declarations
 		public Device_Name Device;
-		#endregion Declarations
 
-		//****************************************************************************************
-		// 
-		//  Device_Name_Transport	-	Constructor
-		// 
-		//****************************************************************************************
-		public Device_Name_Transport(Device_Name Device)
+		public bool IsSocketConnected;
+		public string ControllerHost = string.Empty;
+		public int ControllerPort = 80;
+		public bool UseSsl;
+		public string LastTransportError = string.Empty;
+
+		public Device_Name_Transport(Device_Name device)
 		{
-			#region Debug Message
-			Log("Device_Name_Transport - Constructor - Start");
-			#endregion Debug Message
-
 			IsEthernetTransport = true;
-			IsConnected = true;
-			this.Device = Device;
-
-			#region Debug Message
-			Log("Device_Name_Transport - Constructor - Finish");
-			#endregion Debug Message
+			IsConnected = false;
+			Device = device;
 		}
 
-		#region Overrides
-		//****************************************************************************************
-		// 
-		//  Start	-	
-		// 
-		//****************************************************************************************
+		public void Configure(string host, int port, bool useSsl)
+		{
+			ControllerHost = host ?? string.Empty;
+			ControllerPort = port <= 0 ? 80 : port;
+			UseSsl = useSsl;
+		}
+
+		public string GetSocketUri()
+		{
+			var scheme = UseSsl ? "wss" : "ws";
+			return string.Format("{0}://{1}:{2}", scheme, ControllerHost, ControllerPort);
+		}
+
 		public override void Start()
 		{
-			#region Debug Message
-			Log("Device_Name_Transport - Start - Start");
-			#endregion Debug Message
+			if (string.IsNullOrEmpty(ControllerHost))
+			{
+				LastTransportError = "ControllerHost is required.";
+				IsSocketConnected = false;
+				IsConnected = false;
+				Log("JellyfishLighting - transport start failed: " + LastTransportError);
+				return;
+			}
 
-			#region Debug Message
-			Log("Device_Name_Transport - Start - Finish");
-			#endregion Debug Message
+			LastTransportError = string.Empty;
+			Log("JellyfishLighting - transport start requested for " + GetSocketUri());
+			// TODO: Replace scaffold with an actual WebSocket connection implementation.
+			IsSocketConnected = true;
+			IsConnected = true;
 		}
 
-		//****************************************************************************************
-		// 
-		//  Stop	-	
-		// 
-		//****************************************************************************************
 		public override void Stop()
 		{
-			#region Debug Message
-			Log("Device_Name_Transport - Stop - Start");
-			#endregion Debug Message
-
-			#region Debug Message
-			Log("Device_Name_Transport - Stop - Finish");
-			#endregion Debug Message
+			Log("JellyfishLighting - transport stop requested.");
+			IsSocketConnected = false;
+			IsConnected = false;
 		}
 
-		//****************************************************************************************
-		// 
-		//  SendMethod	-	
-		// 
-		//****************************************************************************************
 		public override void SendMethod(string message, object[] parameters)
 		{
-			#region Debug Message
-			Log("Device_Name_Transport - SendMethod - Start");
-			#endregion Debug Message
+			if (!IsSocketConnected)
+			{
+				Log("JellyfishLighting - SendMethod ignored (socket not connected)");
+				return;
+			}
 
-			#region Debug Message
-			Log("Device_Name_Transport - SendMethod - Finish");
-			#endregion Debug Message
+			Log("JellyfishLighting TX: " + message);
+			// TODO: Send JSON message over websocket stream.
 		}
-		#endregion Overrides
+
+		public void SendJson(string json)
+		{
+			if (string.IsNullOrEmpty(json))
+			{
+				return;
+			}
+
+			SendMethod(json, null);
+		}
 	}
 }
